3f1b2f2051d972a52d3b7f313f0324b423bef63f
Kehan Harman <kehan@gaiaresources.com.au>
2016-03-03 11:00:18 +0800

    Test for individual user interfaces, remove unused user interface (`event_cataloguers_ui`)

--- tests/profile/ProfileUiTest.php
+++ tests/profile/ProfileUiTest.php
@@ -1,11 +1,26 @@
 <?php
 
-class ProfileUiTest extends PHPUnit_Framework_TestCase {
-    public function testProfileContainsUis() {
+class ProfileUiTest extends PHPUnit_Framework_TestCase
+{
+    public function testProfileContainsUis()
+    {
         $basePath = dirname(dirname(__DIR__));
         $doc = new DOMDocument();
         $doc->load("$basePath/profile/rwahs.xml");
         $xpath = new DOMXPath($doc);
-        $this->assertEquals(14, $xpath->query('//userInterfaces/userInterface')->length, 'The number of user interfaces should match');
+        $this->assertEquals(13, $xpath->query('//userInterfaces/userInterface')->length, 'The number of user interfaces should match');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="museum_object_ui"]')->length, 'The user interface with code "museum_object_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="standard_entity_ui"]')->length, 'The user interface with code "standard_entity_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="subject_list_ui"]')->length, 'The user interface with code "subject_list_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="conservation_ui"]')->length, 'The user interface with code "conservation_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="object_subject_list_ui"]')->length, 'The user interface with code "object_subject_list_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="occurrence_subject_list_ui"]')->length, 'The user interface with code "occurrence_subject_list_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="places_ui"]')->length, 'The user interface with code "places_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="standard_collection_ui"]')->length, 'The user interface with code "standard_collection_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="standard_storage_locations_ui"]')->length, 'The user interface with code "standard_storage_locations_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="standard_object_lots_ui"]')->length, 'The user interface with code "standard_object_lots_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="standard_representation_ui"]')->length, 'The user interface with code "standard_representation_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="standard_representation_annotation_ui"]')->length, 'The user interface with code "standard_representation_annotation_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('//userInterface[@code="movement_cataloguers_ui"]')->length, 'The user interface with code "movement_cataloguers_ui" should exist.');
     }
 }
