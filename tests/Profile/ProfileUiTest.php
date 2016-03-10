6da55fa51b9afaa320f34c02fe436351853efe26
Kehan Harman <kehan@gaiaresources.com.au>
2016-03-10 09:56:03 +0800

    more specific xpath selectors

--- tests/Profile/ProfileUiTest.php
+++ tests/Profile/ProfileUiTest.php
@@ -9,21 +9,21 @@ class ProfileUiTest extends AbstractProfileTest
     public function testProfileContainsUis()
     {
         $xpath = $this->xpath;
-        $this->assertEquals(14, $xpath->query('//userInterfaces/userInterface')->length, 'The number of user interfaces should match');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="museum_object_ui"]')->length, 'The user interface with code "museum_object_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="library_object_ui"]')->length, 'The user interface with code "library_object_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="standard_entity_ui"]')->length, 'The user interface with code "standard_entity_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="subject_list_ui"]')->length, 'The user interface with code "subject_list_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="conservation_ui"]')->length, 'The user interface with code "conservation_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="object_subject_list_ui"]')->length, 'The user interface with code "object_subject_list_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="occurrence_subject_list_ui"]')->length, 'The user interface with code "occurrence_subject_list_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="places_ui"]')->length, 'The user interface with code "places_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="standard_collection_ui"]')->length, 'The user interface with code "standard_collection_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="standard_storage_locations_ui"]')->length, 'The user interface with code "standard_storage_locations_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="standard_object_lots_ui"]')->length, 'The user interface with code "standard_object_lots_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="standard_representation_ui"]')->length, 'The user interface with code "standard_representation_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="standard_representation_annotation_ui"]')->length, 'The user interface with code "standard_representation_annotation_ui" should exist.');
-        $this->assertEquals(1, $xpath->query('//userInterfaces/userInterface[@code="movement_cataloguers_ui"]')->length, 'The user interface with code "movement_cataloguers_ui" should exist.');
+        $this->assertEquals(14, $xpath->query('/profile/userInterfaces/userInterface')->length, 'The number of user interfaces should match');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="museum_object_ui"]')->length, 'The user interface with code "museum_object_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="library_object_ui"]')->length, 'The user interface with code "library_object_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="standard_entity_ui"]')->length, 'The user interface with code "standard_entity_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="subject_list_ui"]')->length, 'The user interface with code "subject_list_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="conservation_ui"]')->length, 'The user interface with code "conservation_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="object_subject_list_ui"]')->length, 'The user interface with code "object_subject_list_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="occurrence_subject_list_ui"]')->length, 'The user interface with code "occurrence_subject_list_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="places_ui"]')->length, 'The user interface with code "places_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="standard_collection_ui"]')->length, 'The user interface with code "standard_collection_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="standard_storage_locations_ui"]')->length, 'The user interface with code "standard_storage_locations_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="standard_object_lots_ui"]')->length, 'The user interface with code "standard_object_lots_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="standard_representation_ui"]')->length, 'The user interface with code "standard_representation_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="standard_representation_annotation_ui"]')->length, 'The user interface with code "standard_representation_annotation_ui" should exist.');
+        $this->assertEquals(1, $xpath->query('/profile/userInterfaces/userInterface[@code="movement_cataloguers_ui"]')->length, 'The user interface with code "movement_cataloguers_ui" should exist.');
     }
 
     public function testValidRestrictionsForUserInterfacesAndScreens()
