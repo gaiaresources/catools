00ba6332e52f787b3bec19121eaa1404276b9cf9
Kehan Harman <kehan@gaiaresources.com.au>
2016-03-18 13:12:43 +0800

    Add `photograph` group

--- tests/Profile/ProfileACLTest.php
+++ tests/Profile/ProfileACLTest.php
@@ -7,7 +7,7 @@ class ProfileACLTest extends AbstractProfileTest
     public function testProfileContainsGroups()
     {
         $xpath = $this->xpath;
-        $this->assertEquals(2, $xpath->query('/profile/groups/group')->length, 'The number of groups should match');
+        $this->assertEquals(3, $xpath->query('/profile/groups/group')->length, 'The number of groups should match');
         $this->assertEquals(1, $xpath->query('/profile/groups/group[@code="museum"]')->length, 'The group with code "museum" should exist.');
         $this->assertEquals(1, $xpath->query('/profile/groups/group[@code="library"]')->length, 'The group with code "library" should exist.');
         $this->assertEquals(1, $xpath->query('/profile/groups/group[@code="photograph"]')->length, 'The group with code "photograph" should exist.');
