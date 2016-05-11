70c99b6477ef2940507249a6c791dcef3f5249cd
Kehan Harman <kehan@gaiaresources.com.au>
2016-05-11 13:07:46 +0800

    Remove additional library storage locations that have been removed through data cleaning

--- tests/Profile/LibraryProfileTest.php
+++ tests/Profile/LibraryProfileTest.php
@@ -7,10 +7,19 @@ class LibraryProfileTest extends AbstractProfileTest
 {
     function testLibraryStorageLocationsExist()
     {
-        $locations = array('BattyeLibrary','BoxCollection','Display','FoyerAnnex','LargeBook','Library','MapAnnex','MuseumStore','Passage','PhotographsRoom','Reference','TranbyRoom');
+        $locations = array('BattyeLibrary', 'FoyerAnnex', 'Library', 'MapAnnex', 'Museum', 'Passage', 'PhotographsRoom');
 
         foreach($locations as $location){
             $this->assertEquals(1,$this->xpath->query("/profile/lists/list[@code='LibraryStorageLocations']//item[@idno='$location']")->length, "Need to have a storage location entry for `$location`.");
+        }
+    }
+    function testNoExtraLibraryLocationsExist()
+    {
+        $locations = array('BattyeLibrary', 'FoyerAnnex', 'Library', 'MapAnnex', 'Museum', 'Passage', 'PhotographsRoom');
+        /** @var \DOMElement $location */
+        foreach($this->xpath->query("/profile/lists/list[@code='LibraryStorageLocations']//item") as $location){
+            $id = $location->getAttribute('idno');
+            $this->assertContains($id, $locations, "The location `$id` should not exist.");
         }
     }
 }
